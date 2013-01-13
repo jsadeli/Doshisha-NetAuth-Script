8887c5e62348d8595dc18f659e2516f6bf1d7376
Jeffrey Sadeli <jeffrey@sadeli.com>
2013-01-13 16:01:01 +0900

    Added autocomplete attribute to Doshisha's Network Authentication login form to allow for browser autofill.

---
+++ donet.user.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @name        Doshisha's Network Authentication AutoComplete
+// @author      Jeffrey Sadeli
+// @namespace   Doshisha University Authentication
+// @description Enabled Auto fills for Doshisha's network authentication form
+// @include     https://do-net.doshisha.ac.jp:10443/
+// @include     https://do-netmac.doshisha.ac.jp:10443/auth.html
+// @version     2.0-20130113
+// ==/UserScript==
+
+function initialize() {
+    document.forms[0].setAttribute('target', '_parent');
+    document.forms[0].setAttribute('autocomplete', 'on');
+}
+
+initialize();
